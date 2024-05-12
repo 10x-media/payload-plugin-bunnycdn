e4c4260697d677bcf0ffe3b36630ce2901881507
Sandro Wegmann <sandrowegmann02@gmail.com>
2024-05-12 20:16:19 +0200

    init v3 plugin template

--- dev/src/payload.config.ts
+++ dev/src/payload.config.ts
@@ -1,65 +1,25 @@
-
-import { buildConfig } from 'payload/config';
-import path from 'path';
-import Users from './collections/Users';
-import Examples from './collections/Examples';
+import { buildConfig } from 'payload/config'
+import path from 'path'
+import Users from './collections/Users'
+import Examples from './collections/Examples'
 import { mongooseAdapter } from '@payloadcms/db-mongodb'
-import { webpackBundler } from '@payloadcms/bundler-webpack'
-import { slateEditor } from '@payloadcms/richtext-slate'
-import { pluginBunnyCdn } from '../../src/index'
-import { Media } from './collections/Media';
+import { lexicalEditor } from '@payloadcms/richtext-lexical'
+import { samplePlugin } from '../../src/index'
+import sharp from 'sharp'
 
 export default buildConfig({
+  secret: process.env.PAYLOAD_SECRET || '',
   admin: {
     user: Users.slug,
-    bundler: webpackBundler(),
-    webpack: config => {
-      const newConfig = {
-        ...config,
-        resolve: {
-          ...config.resolve,
-          alias: {
-            ...(config?.resolve?.alias || {}),
-            react: path.join(__dirname, '../node_modules/react'),
-            'react-dom': path.join(__dirname, '../node_modules/react-dom'),
-            payload: path.join(__dirname, '../node_modules/payload'),
-          },
-        },
-      }
-      return newConfig
-    },
   },
-  editor: slateEditor({}),
-  collections: [
-    Examples, Users, Media
-  ],
+  editor: lexicalEditor({}),
+  collections: [Examples, Users],
   typescript: {
     outputFile: path.resolve(__dirname, 'payload-types.ts'),
   },
-  graphQL: {
-    schemaOutputFile: path.resolve(__dirname, 'generated-schema.graphql'),
-  },
-  plugins: [pluginBunnyCdn({
-    credentials: {
-      hostname: process.env.PAYLOAD_PUBLIC_BUNNY_CDN_REGION ? `${process.env.PAYLOAD_PUBLIC_BUNNY_CDN_REGION}.${(process.env.PAYLOAD_PUBLIC_BUNNY_CDN_BASE_HOSTNAME || 'storage.bunnycdn.com')}` : (process.env.PAYLOAD_PUBLIC_BUNNY_CDN_BASE_HOSTNAME || 'storage.bunnycdn.com'),
-      storageZoneName: process.env.PAYLOAD_PUBLIC_BUNNY_CDN_STORAGE_ZONE_NAME,
-      pullZoneName: process.env.PAYLOAD_PUBLIC_BUNNY_CDN_PULL_ZONE_NAME,
-      accessKey: process.env.BUNNY_CDN_API_KEY,
-    },
-    collections: {
-      media: {
-        prefix: 'media',
-        watermark: false,
-        imageSizes: {
-          'croppedWatermarked': {
-            watermark: true,
-            watermarkImagePath: path.resolve(__dirname, 'instyle-watermark.png'),
-          }
-        }
-      }
-    },
-  })],
+  plugins: [samplePlugin({ enabled: true })],
   db: mongooseAdapter({
-    url: process.env.DATABASE_URI,
+    url: process.env.DATABASE_URI || '',
   }),
+  sharp,
 })
